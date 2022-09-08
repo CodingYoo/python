c19ea020edb861ead365137dd9a6ab28c4d0d8a0
CodingYoo <doslphx@gmail.com>
2022-09-08 11:53:29 +0800

    String type

--- data_types/cy_string.py
+++ data_types/cy_string.py
@@ -1,3 +1,19 @@
+'''
+@file       :cy_string.py
+@desc       :python 字符串
+@time       :2022/09/08 11:48:08
+@author     :cy
+@version    :1.0
+'''
+
 var1 = 'python'
-print(var1)
+
+# python
+print(var1) 
+# p
 print(var1[0:1])
+# p
+print(var1[:1])
+# n
+print(var1[-1])
+
